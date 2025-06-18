18249039e2b7e0809419d8d0ceb6758d5159c20a
Juan Manuel Rodriguez <jrodriguez2232@cue.edu.co>
2025-06-17 19:09:42 -0500

    feat: configuración inicial del proyecto React

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import DocentesModule from './components/docentes/DocentesModule';
+
+function App() {
+  return <DocentesModule />;
+}
+
+export default App;
